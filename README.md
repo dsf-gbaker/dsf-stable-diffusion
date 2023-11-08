f1759bbf8a9a1f503d643670fcb0260524961641
beerskunk <6002108+beerskunk@users.noreply.github.com>
2023-11-08 14:58:46 -0500

    Stable Diffusion and UI
    Motivation
    ---
    I wanted an easy to use local flavor of stable-diffusion with a simple UI.
    
    Modifications
    ---
    - Added UI
    - Added docker-compose to run backend and frontend

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # dsf-stable-diffusion
 A Dockerized local stable diffusion with Angular UI
+
+# Instructions
+- Install [Docker Desktop](https://www.docker.com/products/docker-desktop/)
+
+- Make sure *Docker Desktop* is running
+
+- [Clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) this repository
+  - NOTE: When cloning this repository to your local drive, please make a note WHERE you cloned the files (ex. ~/Documents/dsf-stable-diffusion)
+
+>  ## MAC OSX
+>  - Open a [Terminal](https://support.apple.com/guide/terminal/open-or-quit-terminal-apd5265185d-f365-44cb-8b09-71a064a42125/mac)
+>   - In the Terminal window, change to the directory where you cloned the project
+>     - ex. `cd ~/thefolder/where/you/cloned/dsf-stable-diffusion`
+> - Start up LocalAI by entering the following command
+>    - `docker compose up --build -d`
+>  - After a few seconds, open a browser to [http://localhost:4200](http://localhost:4200)
+>  - Have fun making images!
+
+> ## WINDOWS
+>   - Open [Powershell](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.3)
+>     - WIN Key + R
+>     - type in `powershell`
+>     - press enter
+>     - This should open a powershell window
+>   - In the Powershell window, change to the directory where you cloned the project
+>     - `cd ~/thefolder/where/you/cloned/dsf-stable-diffusion`
+> - Start up LocalAI by entering the following command
+>    - `docker compose up --build -d`
+>  - After a few seconds, open a browser to [http://localhost:4200](http://localhost:4200)
+>  - Have fun making images!
---
+++ ui/src/app/components/main/main.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { AlertType } from '../alert/alert.component';
+import { EImageSize, ImageProvider } from 'src/app/interfaces/imageprovider.interface';
+import { LLamaImageProviderService } from 'src/app/services/llamaimageprovider/llamaimageprovider.service';
+import { EMPTY, catchError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+  providers: [{ provide: ImageProvider, useClass: LLamaImageProviderService }]
+})
+export class MainComponent {
+
+  placeholder     = 'Enter your prompt here...';
+  negplaceholder  = 'Enter a negative prompt here to further refine what you do NOT want.';
+  alertType       = AlertType.none;
+  prompt          = '';
+  negprompt       = '';
+  alertMsg        = '';
+  img             = '';
+  progress        = 100;
+  maxprogress     = 100;
+  working         = false;
+
+  constructor(private imager: ImageProvider) {}
+
+  onDismissAlert(val?: any) {
+    this.alertMsg = '';
+    this.alertType = AlertType.none;
+  }
+
+  public onGenerateImage() {
+
+    this.onDismissAlert();
+    this.prompt.trim();
+    this.negprompt.trim();
+    let prompt = this.prompt;
+
+    if (prompt.length <= 0) {
+      this.alertType = AlertType.warning;
+      this.alertMsg = 'Prompt is empty...please enter some text.'
+      return;
+    }
+    
+    if (this.negprompt.length > 0) {
+        prompt += '|' + this.negprompt;
+    }
+
+    this.working = true;
+
+    // TODO: make image size dynamic
+    this.imager.createImage(prompt, EImageSize.small)
+      .pipe(catchError((e: HttpErrorResponse, src) => { 
+          console.log(JSON.stringify(e));
+          this.working = false;
+
+          let msg = 'HTTP ' + e.status.toString() + ' :: ';
+
+          if (e.status === 0) {
+            msg += 'Unable to connect to the server. Is your LLama Docker image running?';
+          }
+          else {
+            msg += e.message;
+          }
+
+          this.alertMsg   = msg;
+
+          this.alertType  = AlertType.error;
+          return EMPTY;
+        })
+      )
+      .subscribe(res => {
+        console.log('Response Recieved...');
+        console.log(JSON.stringify(res));
+        this.working = false;
+        this.img = res.filelocation;
+      });
+  }
+}
